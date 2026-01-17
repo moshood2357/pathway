95e553bf850a8cae6962a4ed4983f9d1ce2c08d8
Moshood Sanusi <sanusimoshoodabiola@gmail.com>
2026-01-17 16:39:51 +0100

    Update ADMIN_EMAIL and comment out DB URIs

--- website/config.py
+++ website/config.py
@@ -1,10 +1,18 @@
+import os 
+
+
 class GeneralConfig:
-    ADMIN_EMAIL='admin@foodcircle.com'
+    ADMIN_EMAIL = os.environ.get('ADMIN_EMAIL', 'admin@pathway.com')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 class TestConfig(GeneralConfig):
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root@localhost/path_db"
+#     # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root@localhost/path_db"
     SQLALCHEMY_TRACK_MODIFICATIONS=False
 
 class LiveConfig(GeneralConfig):
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root@localhost/path_db"
-    SQLALCHEMY_TRACK_MODIFICATIONS=False 
+    # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root@localhost/path_db"
+    DEBUG = False
+    TESTING = False
+    
+    
